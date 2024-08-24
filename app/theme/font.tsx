99d343ceb22c3ec4cb7a6e1b5588cd60cbc65f14
ShelOrock <orockshel@gmail.com>
2024-08-24 03:15:03 -0400

    added function to add margins to elements

---
+++ app/theme/font.tsx
@@ -0,0 +1,12 @@
+const font = {
+  fontFamily: "Titillium Web",
+  fontSizes: {
+    title: "1.8rem",
+    large: "1.5rem",
+    normal: "1rem",
+    small: "0.7rem",
+    default: "1rem"
+  }
+};
+
+export default font;

7ebf5f0c481335647067b832460807d4e24a38f5
ShelOrock <orockshel@gmail.com>
2024-08-31 23:05:51 -0400

    began writing collision for rooms and added roofs

---
+++ app/components/Roof/Roof.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Roof = () => (
+
+);
+
+export default Roof;
--- app/types/Entities/index.tsx
+++
@@ -1,4 +0,0 @@
-export * as EntityTypes from "./Entity";
-export * as RoomTypes from "./Room";
-export * as DoorTypes from "./Door";
-export * as WindowTypes from "./Window";

61c17e57b6f86e9044f1d8a8873f5187801b6e36
ShelOrock <orockshel@gmail.com>
2024-10-05 23:49:59 -0400

    added function to check orientation of roof preview and turn it accordingly

--- app/components/RoofPreview/RoofPreview.tsx
+++ app/components/RoofPreview/RoofPreview.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ComponentPropTypes } from "./types";
 import { Group, Line, Rect } from "react-konva";
+import { Directions } from "../../enums";
 
 const RoofPreview: React.FC<ComponentPropTypes> = ({
   xPosition,
@@ -12,24 +13,37 @@ const RoofPreview: React.FC<ComponentPropTypes> = ({
   const ROOF_PREVIEW_WIDTH = 100;
   const ROOF_PREVIEW_HEIGHT = 100;
 
+  const evaluateLineOrientation = () => {
+    if(orientation === Directions.horizontal) {
+      return [
+        xPosition,
+        yPosition + 50,
+        xPosition + ROOF_PREVIEW_WIDTH,
+        yPosition + 50
+      ];
+    };
+
+    return [
+      xPosition + 50,
+      yPosition,
+      xPosition + 50,
+      yPosition + ROOF_PREVIEW_HEIGHT
+    ];
+  }
+
   return (
     <Group>
       <Rect
         x={ xPosition }
         y={ yPosition }
-        width={ ROOF_PREVIEW_WIDTH }
-        height={ ROOF_PREVIEW_HEIGHT }
+        width={orientation === Directions.horizontal ? ROOF_PREVIEW_WIDTH : ROOF_PREVIEW_HEIGHT }
+        height={ orientation === Directions.horizontal ? ROOF_PREVIEW_HEIGHT : ROOF_PREVIEW_WIDTH }
         stroke={ "#000" }
         strokeWidth={ 5 }
         dash={ [ 10, 5 ] }
       />
       <Line
-        points={ [
-          xPosition,
-          yPosition + 50,
-          xPosition + ROOF_PREVIEW_WIDTH,
-          yPosition + 50,
-        ] }
+        points={ evaluateLineOrientation() }
         stroke={ "#000" }
         strokeWidth={ 2 }
       />
